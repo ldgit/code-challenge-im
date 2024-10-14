cc299835a9d0724a73bbc871c82f44912021b794
Danko Lučić <ld.is.a.git@gmail.com>
2024-10-14 21:44:32 +0200

    rewrite url parser algorithm so it can detect multiple urls in the parsed text

--- test/parseUrlFromText.test.js
+++ test/parseUrlFromText.test.js
@@ -13,25 +13,39 @@ test('parseUrlFromText should return empty array for text that has url outside s
   expect(parseUrlFromText('url www.google.com outside [] brackets')).toEqual([]);
 });
 
-test('parseUrlFromText should return empty array for text that has url inside invalid square brackets', () => {
-  expect(parseUrlFromText('url ]www.google.com outside [ brackets')).toEqual([]);
-  expect(parseUrlFromText('url [ www.google.com outside brackets')).toEqual([]);
+[
+  'url ]www.google.com outside [ brackets',
+  'url [ www.google.com outside brackets',
+].forEach((text) => {
+  test(`parseUrlFromText should return empty array for text that has url inside invalid square brackets ${text}`, () => {
+    expect(parseUrlFromText(text)).toEqual([]);
+  });
 });
 
-test('parseUrlFromText should return empty array for text that has url inside escaped square brackets', () => {
-  expect(parseUrlFromText("url \\[ www.google.com ]")).toEqual([]);
-  expect(parseUrlFromText("url [ www.google.com \\]")).toEqual([]);
+[
+  "url \\[ www.google.com ]",
+  "url \\\\[ www.google.com ]",
+  "url [ www.google.com \\]",
+  "url [ www.google.com \\\\]",
+].forEach((text) => {
+  test(`parseUrlFromText should return empty array for text that has url inside escaped square brackets "${text}"`, () => {
+    expect(parseUrlFromText(text)).toEqual([]);
+  });
 });
 
 [
   ["url [ www.google.com ]", 'www.google.com'],
   ["url [ google.com ]", 'google.com'],
+  ["url ] []] [ google.com ]", 'google.com'],
   ["url with [ extra text www.example.com inside brackets ]", 'www.example.com'],
   ["multiple urls [bla www.first.com more text www.second.com other stuff]", 'www.second.com'],
   ["multiple levels[ www.first.com www.second.com]", 'www.second.com'],
   ["multiple levels[ [www.first.com] www.second.com]", 'www.second.com'],
   ["multiple levels[ www.first.com [www.third.com]]", 'www.third.com'],
   ["multiple urls [ www.first.com] some text [ www.third.com ]]", 'www.third.com'],
+  // Only the outermost bracket should count.
+  ["multiple urls [[ www.first.com] some text [www.third.com]]", 'www.third.com'],
+  ["multiple urls [ www.first.com] some [[www.second.com] text [www.third.com]]", 'www.third.com'],
 ].forEach(([text, expectedUrl]) => {
   test(`parseUrlFromText should return array with single url that is inside square brackets "${text}"`, () => {
     expect(parseUrlFromText(text)).toEqual([expectedUrl]);
@@ -40,9 +54,12 @@ test('parseUrlFromText should return empty array for text that has url inside es
 
 [
   ["multiple urls [ www.first.com] [www.third.com]", ['www.first.com', 'www.third.com']],
+  ["multiple urls [ www.first.com] [][][][]] [www.third.com]", ['www.first.com', 'www.third.com']],
+  ["multiple urls [google.com][][][] [ www.first.com] ] [www.third.com]", ['www.first.com', 'www.third.com']],
   ["multiple urls [ www.first.com] some text [www.third.com]", ['www.first.com', 'www.third.com']],
+  ["multiple urls [ www.first.com] some [www.second.com] text [www.third.com]", ['www.first.com', 'www.second.com', 'www.third.com']],
 ].forEach(([text, expectedUrl]) => {
-  test.skip(`parseUrlFromText should return array with multiple urls that are inside different square brackets "${text}"`, () => {
-    expect(parseUrlFromText(text)).toEqual([expectedUrl]);
+  test(`parseUrlFromText should return array with multiple urls that are inside different square brackets "${text}"`, () => {
+    expect(parseUrlFromText(text)).toEqual(expectedUrl);
   });
 });
