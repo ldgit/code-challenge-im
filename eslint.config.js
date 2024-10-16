e11fe2492976cc6ff60889610a1befd36e8ed740
Danko Lučić <ld.is.a.git@gmail.com>
2024-10-16 09:54:33 +0200

    adds eslint checks for unit tests

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,21 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
+import vitest from "@vitest/eslint-plugin";
 
 export default [
 	pluginJs.configs.recommended,
 	eslintConfigPrettier,
+	{
+		files: ["test/**"],
+		plugins: {
+			vitest,
+		},
+		rules: {
+			...vitest.configs.recommended.rules,
+			"vitest/no-focused-tests": ["error", { fixable: false }],
+		},
+	},
 	{ languageOptions: { globals: globals.node } },
 	{
 		ignores: ["coverage/*"],
