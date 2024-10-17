0dabc3c77675e0e9180104ae1b1660d3a466b1e9
Danko Lučić <ld.is.a.git@gmail.com>
2024-10-17 10:27:05 +0200

    adds some comments for clarification

--- src/callQueue.js
+++ src/callQueue.js
@@ -10,7 +10,9 @@ import { createSilentLogger } from "./logger.js";
 export function createCallQueue(options) {
 	// Override default options with those provided through options argument, if available.
 	const { requestDelay, retryDelay, fetchUrlData, logger } = {
+		// 1 second
 		requestDelay: 1000,
+		// 1 minute
 		retryDelay: 60000,
 		fetchUrlData: fetch,
 		logger: createSilentLogger(),
@@ -20,6 +22,11 @@ export function createCallQueue(options) {
 	const alreadyCalled = new Map();
 	let requestDelayExpired = true;
 
+	/**
+	 * Calls the url and starts a one second timer when no other calls can be made.
+	 *
+	 * @param {string} url
+	 */
 	function callAndStartTimer(url) {
 		fetchUrlData(url)
 			.then((responseBody) => {
@@ -30,8 +37,11 @@ export function createCallQueue(options) {
 					logger.error(error);
 					return;
 				}
+
+				// In case of failed fetch we retry once after retryDelay of 1 minute.
 				setTimeout(() => {
 					fetchUrlData(url).catch(() => {
+						// If fetch fails twice log the error to stderr.
 						console.error(`Http request failed for ${url}`);
 					});
 				}, retryDelay);
@@ -47,6 +57,7 @@ export function createCallQueue(options) {
 		}, requestDelay);
 	}
 
+	/** Makes a http request to next url in the call queue, if one exists. */
 	function callNextInQueue() {
 		const next = callQueue.entries().next().value;
 		if (!next) {
@@ -56,15 +67,14 @@ export function createCallQueue(options) {
 	}
 
 	return {
-		/**
-		 * @param {string} url
-		 */
+		/** @param {string} url */
 		add(url) {
 			if (alreadyCalled.has(url)) {
 				return;
 			}
 
 			callQueue.set(url);
+			// If last (non retry) call was made more than a second ago, fetch this url immediately.
 			if (requestDelayExpired) {
 				callAndStartTimer(url);
 			}
